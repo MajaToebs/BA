87cc0813aa7caae512c7abc3aed9c452a2a6e4e1
Maja Toebs <maja.toebs@posteo.de>
2019-05-03 15:12:10 +0200

    Preprocessing of homeworks further improved by filtering more bad stuff out

--- preprocessHomework.py
+++ preprocessHomework.py
@@ -4,82 +4,95 @@ from nltk.tokenize import sent_tokenize
 import re
 import os
 
-#for f in os.listdir("Data/homework/essays"):
-#    process(f, "PreprocessedData/homework/essays/f.txt")
 
-#def process(input_filename)
-# the second argument is the input document and the third document the output argument
-if len(sys.argv) == 3:
-    input_filename = sys.argv[1]
-    output_filename = sys.argv[2]
+def process(input_filename, output_filename):
+    # read in the given document
+    with open(input_filename, "r", encoding="utf-8-sig") as raw:
+        text_in = raw.readlines()
+        raw.close()
 
-# read in the given document
-with open(input_filename, "r", encoding="utf-8-sig") as raw:
-    text_in = raw.read().replace("\n", " ")
-    raw.close()
+    content = ""
+    for line in text_in:
+        # if the bibliography starts, we ignore the rest of the document
+        if bool(re.search(r"^bibliography:?\s*$", line.lower())) or \
+            bool(re.search(r"^references:?\s*$", line.lower())):
+            break
 
-# tokenize the given text into sentences with nltk, sentences is then a list of lists consisting of strings
-sentences = sent_tokenize(text_in)
+        # if a line starts with a comment or formatting from a reader or contains meta-information about the document, ignore that line
+        if not (bool(re.search(r"^kommentiert", line.lower())) or \
+                bool(re.search(r"^formatiert", line.lower())) or \
+                bool(re.search(r"^formatted", line.lower())) or \
+                bool(re.search(r"^commented", line.lower()))  or \
+                bool(re.search(r"^words count", line.lower()))  or \
+                bool(re.search(r"^(\W|\d|_)+$", line.lower()))  or \
+                bool(re.search(r"^word count", line.lower())) or \
+                bool(re.search(r"^date of submission", line.lower())) or \
+                bool(re.search(r"^signature:?", line.lower())) or \
+                bool(re.search(r"^\d+ words\s*$", line.lower())) or \
+                bool(re.search(r"^==.+==$", line.lower())) ):
+            content += line
 
-# split each sentence into words and filter out different things
-for index, sentence in enumerate(sentences):
-    #print(sentence)
+    # some documents do not end with a final dot, but an end of sentence is needed for application of the readability indices
+    if content[-1] != ".":
+        content += "."
 
-    # if the bibliography starts, we ignore the rest of the document
-    if "bibliography" in sentence.lower() or "references" in sentence.lower():
-        sentences = sentences[0:index]
-        break
+    # tokenize the given text into sentences with nltk, sentences is then a list of lists consisting of strings
+    content = content.replace("\n", " ")
+    sentences = sent_tokenize(content)
 
-    # if a source is mentioned in parentheses, we substitute that part of the sentence with an empty string
-    sentence = re.sub(r"\(.*\d+.*\)", " ", sentence)
-    # if a source is mentioned in parentheses with " but no date, we substitute that part of the sentence with an empty string
-    sentence = re.sub(r'\(.*[‘’“”\'\"].+[‘’“”\'\"].*\)', " ", sentence)
-    # delete page numbers
-    sentence = re.sub(r" \d+  ", " ", sentence)
+    # filter out sources, urls, page numbers
+    for index, sentence in enumerate(sentences):
+        # if a source is mentioned in parentheses, we substitute that part of the sentence with an empty string
+        sentence = re.sub(r"\(.*\d+.*\)", " ", sentence)
+        # if a source is mentioned in parentheses with " but no date, we substitute that part of the sentence with an empty string
+        sentence = re.sub(r'\(.*[‘’“”\'\"].+[‘’“”\'\"].*\)', " ", sentence)
+        # delete page numbers
+        sentence = re.sub(r" \d+  ", " ", sentence)
 
-    # if a sentence contains a comment from a reader, delete that sentence
-    if "kommentiert" in sentence.lower():
-        sentence = " "
+        # split the sentence into words
+        words = sentence.split(" ")
+        # delete urls
+        words = [w for w in words if bool(re.search(r"^http.+", w)) is False]
+        # delete strange symbols like arrows at the beginning of a word
+        words_without_symbols = []
+        for w in words:
+            if bool(re.search(r"^\W\w+", w)) is True:
+                words_without_symbols.append(w[1:])
+            else:
+                words_without_symbols.append(w)
+        words = words_without_symbols
 
+        #words = [w for w in words if bool(re.search(r"(^\W+$|([a-zA-Z]*\d+[a-zA-Z]*)+)", w)) is False]
 
-    words = sentence.split(" ")
-    # delete urls
-    words = [w for w in words if bool(re.search(r"^http.+", w)) is False]
-    # delete page breaks
-    words = [w for w in words if bool(re.search(r"^==.+==$", w)) is False]
+        # store the shortened sentences
+        sentences[index] = words
 
+    # open file where we put the output to write into it
+    text_out = open(output_filename, "w")
 
+    # iterate over sentences to filter out the ones which are too short to be real sentences
+    for i, s in enumerate(sentences):
+        # filter out sentences that are shorter than three words
+        if (len(s) >= 3):
+            for w in s:
+                # filter out sentences that contain only whitespaces
+                if len(w)>0:
+                    text_out.write(w)
+                    # separate words with white spaces
+                    text_out.write(" ")
+            # separate sentences with line breaks
+            text_out.write('\n')
 
-
-    # filter out special characters (non-word, e.g. "&")
-
-
-    #words = [w for w in words if bool(re.search(r"^\W+$", w)) is False]
-    #print(words)
-    # filter out digits, numbers, floating point numbers, version numbers etc
-    #words = [w for w in words if bool(re.search(r"^(\W|\d)+$", w)) is False]
-    #print(words)
-    #words = [w for w in words if bool(re.search(r"(^\W+$|([a-zA-Z]*\d+[a-zA-Z]*)+)", w)) is False]
-    #print(words)
-
-    # store the shortened sentences
-    sentences[index] = words
-
-# open file where we put the output
-text_out = open(output_filename, "w")
-
-
-
-# iterate over sentences to filter those
-for i, s in enumerate(sentences):
-    # filter out sentences that are shorter than three words
-    if (len(s) >= 3):
-        # filter out sentences that contain only whitespaces
-        for w in s:
-            if len(w)>0:
-                text_out.write(w)
-                text_out.write(" ")
-        text_out.write('\n')
+    text_out.close()
 
 
-text_out.close()
+
+# executes for all files in one directory
+for f in os.listdir("Data/homework/essays"):
+    process("Data/homework/essays/" + f, "PreprocessedData/homework/essays/" + f + ".txt")
+for f in os.listdir("Data/homework/aff-case"):
+    process("Data/homework/aff-case/" + f, "PreprocessedData/homework/aff-case/" + f + ".txt")
+for f in os.listdir("Data/homework/osmosis"):
+    process("Data/homework/osmosis/" + f, "PreprocessedData/homework/osmosis/" + f + ".txt")
+for f in os.listdir("Data/homework/mission-command"):
+    process("Data/homework/mission-command/" + f, "PreprocessedData/homework/mission-command/" + f + ".txt")
