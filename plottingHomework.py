3f5c6eff42a8bc68fba0337133f92a0a2d6cc930
Maja Toebs <maja.toebs@posteo.de>
2019-05-31 16:16:23 +0200

    Plotting improved with median and deviation in addition to mean and variance

--- plottingHomework.py
+++ plottingHomework.py
@@ -2,6 +2,138 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
+# BOXPLOT
+# show all the boxplots of all documents' GFIs for chunk size m next to each other
+def plot_boxplot():
+    for m in [40, 35, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10]: # setting m to a high number doesn't make sense for boxplots
+        my_doc_chunk_m = df.loc[df['length_of_chunk'] == m]
+        my_doc_chunk_m.boxplot(column='GFI', by='document', figsize=(14,7))
+        plt.ylabel('GFI')
+        plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
+        plt.savefig('Plots/homework/boxplot/' + str(m) + 'longChunksBoxplottedAllHomeworks.svg')
+
+
+
+
+
+# BAR CHART
+def plot_barchart():
+    for homework in all_homeworks:
+        for m in [20, 18, 16, 14, 12, 10]: # doesn't make sense with bigger m, since there is only one bar then
+            plt.close('all')
+            this_doc_len_m_ = df.loc[df['document'] == homework]
+            this_doc_len_m = this_doc_len_m_.loc[df['length_of_chunk'] == m]
+            if this_doc_len_m.empty == True:
+                continue
+            ints = []
+            for i in this_doc_len_m['number_of_chunk']:
+                ints.append(int(i))
+            if len(ints) < 6:
+                continue
+            plt.bar(ints,this_doc_len_m['GFI'])
+            plt.gcf().set_size_inches(7, 3.5)
+            plt.ylabel('GFI')
+            plt.xlabel('number of chunk (from beginning to end)')
+            plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
+            plt.savefig('Plots/homework/bar/' + str(m) + "_long_" + homework[-6:] + 'indicesOverNumberOfChunk.svg')
+
+# SCATTER PLOT
+def plot_scatterplot():
+    for homework in all_homeworks:
+        plt.close('all')
+        this_doc = df.loc[df['document'] == homework]
+
+        # not interesting to plot, since there are just too few values
+        if len(this_doc['length_of_chunk']) < 20:
+            continue
+
+        # scatter the GFIs depending on the chunk size
+        plt.scatter(this_doc['length_of_chunk'], this_doc['GFI'])
+        plt.gcf().set_size_inches(14, 7)
+
+        # calculate the mean GFIs for each chunk size
+        mean_GFIs = {'len': [],
+                     'mean': []}
+        for l in sorted(set(this_doc['length_of_chunk'])):
+            values = this_doc.loc[this_doc['length_of_chunk'] == l]['GFI']
+            mean_GFIs['len'].append(l)
+            mean_GFIs['mean'].append(np.mean(values))
+
+        # insert the means into the plot
+        plt.plot(mean_GFIs['len'], mean_GFIs['mean'], 'k-', color='r')
+        plt.ylabel('GFI')
+        plt.xlabel('length of chunk in sentences')
+        plt.savefig('Plots/homework/scatter/' + homework[-11:-4] + 'indicesOverChunkSize.svg')
+
+# VARIANCES
+# display variances of chunks
+# read in the variances from the file
+def plot_variances():
+    plt.close('all')
+    df_var_0 = pd.read_csv("variancesHomework_0.csv", header=0, index_col=0)
+    df_var_1 = pd.read_csv("variancesHomework_1.csv", header=0, index_col=0)
+    #df_var_2 = pd.read_csv("variancesHomework_2.csv", header=0, index_col=0) OSMOSIS
+    df_var_3 = pd.read_csv("variancesHomework_3.csv", header=0, index_col=0)
+
+    df_variances = pd.concat([df_var_0, df_var_1, df_var_3], ignore_index=True)
+
+    # get the chunk sizes and their mean variances
+    chunk_sizes = list(sorted(set(df_variances['length_of_chunk'])))
+    for i,c in enumerate(chunk_sizes):
+        chunk_sizes[i] = int(c)
+    mean_chunk_variances = []
+    median_chunk_variances = []
+    # calculate the mean variance for each chunk size
+    for size in chunk_sizes:
+        mean_chunk_variances.append(np.mean(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
+        median_chunk_variances.append(np.median(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
+
+    # scatter them nicely
+    plt.scatter(chunk_sizes, mean_chunk_variances, label = 'mean values')
+    plt.scatter(chunk_sizes, median_chunk_variances, label = 'median values')
+    plt.legend()
+    plt.gcf().set_size_inches(14, 7)
+    plt.ylabel('mean variance of the GFI over all documents')
+    plt.xlabel('length of chunk in sentences')
+    plt.savefig('Plots/homework/variances.svg')
+
+
+# VARIANCES
+# display variances of chunks
+# read in the variances from the file
+def plot_deviations():
+    plt.close('all')
+    df_var_0 = pd.read_csv("variancesHomework_0.csv", header=0, index_col=0)
+    df_var_1 = pd.read_csv("variancesHomework_1.csv", header=0, index_col=0)
+    # df_var_2 = pd.read_csv("variancesHomework_2.csv", header=0, index_col=0) OSMOSIS
+    df_var_3 = pd.read_csv("variancesHomework_3.csv", header=0, index_col=0)
+
+    df_deviations = pd.concat([df_var_0, df_var_1, df_var_3], ignore_index=True)
+
+    # get the chunk sizes and their mean variances
+    chunk_sizes = list(sorted(set(df_deviations['length_of_chunk'])))
+    for i, c in enumerate(chunk_sizes):
+        chunk_sizes[i] = int(c)
+    mean_chunk_deviations = []
+    median_chunk_deviations = []
+    # calculate the mean deviation for each chunk size
+    for size in chunk_sizes:
+        mean_chunk_deviations.append(np.mean(df_deviations.loc[df_deviations['length_of_chunk'] == size]['std']))
+        median_chunk_deviations.append(np.median(df_deviations.loc[df_deviations['length_of_chunk'] == size]['std']))
+
+    # scatter them nicely
+    plt.scatter(chunk_sizes, mean_chunk_deviations, label='mean values')
+    plt.scatter(chunk_sizes, median_chunk_deviations, label='median values')
+    plt.legend()
+    plt.gcf().set_size_inches(14, 7)
+    plt.ylabel('mean deviations of the GFI over all documents')
+    plt.xlabel('length of chunk in sentences')
+    plt.savefig('Plots/homework/deviations.svg')
+
+
+
 df_0 = pd.read_csv("results_0.csv", header=0, index_col=0)
 df_1 = pd.read_csv("results_1.csv", header=0, index_col=0)
 df_2 = pd.read_csv("results_2.csv", header=0, index_col=0)
@@ -9,17 +141,6 @@ df_3 = pd.read_csv("results_3.csv", header=0, index_col=0)
 
 # concatenate the results of the separate folders with documents
 df = pd.concat([df_0, df_1, df_2, df_3], ignore_index=True)
-
-
-# BOXPLOT
-# show all the boxplots of all documents' GFIs for chunk size m next to each other
-for m in [40, 35, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10]: # setting m to a high number doesn't make sense for boxplots
-    my_doc_chunk_m = df.loc[df['length_of_chunk'] == m]
-    my_doc_chunk_m.boxplot(column='GFI', by='document', figsize=(14,7))
-    plt.ylabel('GFI')
-    plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
-    plt.savefig('Plots/homework/boxplot/' + str(m) + 'longChunksBoxplottedAllHomeworks.svg')
-
 
 all_homeworks = ['aff-case/9536669.txt', 'aff-case/9514020.txt', 'aff-case/9534163.txt', 'aff-case/9534039.txt',
                  'aff-case/9535872.txt', 'aff-case/9536689.txt', 'aff-case/9533580.txt', 'aff-case/9535758.txt',
@@ -51,80 +172,10 @@ all_homeworks = ['aff-case/9536669.txt', 'aff-case/9514020.txt', 'aff-case/95341
                  'mission-command/9489819.txt', 'mission-command/9461911.txt', 'mission-command/9489857.txt',
                  'mission-command/9490214.txt', 'mission-command/9489842.txt']
 
-
-
-# BAR CHART
-for homework in all_homeworks:
-    for m in [20, 18, 16, 14, 12, 10]: # doesn't make sense with bigger m, since there is only one bar then
-        plt.close('all')
-        this_doc_len_m_ = df.loc[df['document'] == homework]
-        this_doc_len_m = this_doc_len_m_.loc[df['length_of_chunk'] == m]
-        if this_doc_len_m.empty == True:
-            continue
-        ints = []
-        for i in this_doc_len_m['number_of_chunk']:
-            ints.append(int(i))
-        if len(ints) < 6:
-            continue
-        plt.bar(ints,this_doc_len_m['GFI'])
-        plt.gcf().set_size_inches(7, 3.5)
-        plt.ylabel('GFI')
-        plt.xlabel('number of chunk (from beginning to end)')
-        plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
-        plt.savefig('Plots/homework/bar/' + str(m) + "_long_" + homework[-6:] + 'indicesOverNumberOfChunk.svg')
-
-
-# SCATTER PLOT
-for homework in all_homeworks:
-    plt.close('all')
-    this_doc = df.loc[df['document'] == homework]
-
-    # not interesting to plot, since there are just too few values
-    if len(this_doc['length_of_chunk']) < 20:
-        continue
-
-    # scatter the GFIs depending on the chunk size
-    plt.scatter(this_doc['length_of_chunk'], this_doc['GFI'])
-    plt.gcf().set_size_inches(14, 7)
-
-    # calculate the mean GFIs for each chunk size
-    mean_GFIs = {'len': [],
-                 'mean': []}
-    for l in sorted(set(this_doc['length_of_chunk'])):
-        values = this_doc.loc[this_doc['length_of_chunk'] == l]['GFI']
-        mean_GFIs['len'].append(l)
-        mean_GFIs['mean'].append(np.mean(values))
-
-
-    # insert the means into the plot
-    plt.plot(mean_GFIs['len'], mean_GFIs['mean'], 'k-', color='r')
-    plt.ylabel('GFI')
-    plt.xlabel('length of chunk in sentences')
-    plt.savefig('Plots/homework/scatter/' + homework[-11:-4] + 'indicesOverChunkSize.svg')
+plot_boxplot()
+plot_barchart()
+plot_scatterplot()
+plot_variances()
+plot_deviations()
 
 plt.close('all')
-# VARIANCES
-# display variances of chunks
-# read in the variances from the file
-df_var_0 = pd.read_csv("variancesHomework_0.csv", header=0, index_col=0)
-df_var_1 = pd.read_csv("variancesHomework_1.csv", header=0, index_col=0)
-#df_var_2 = pd.read_csv("variancesHomework_2.csv", header=0, index_col=0) OSMOSIS
-df_var_3 = pd.read_csv("variancesHomework_3.csv", header=0, index_col=0)
-
-df_variances = pd.concat([df_var_0, df_var_1, df_var_3], ignore_index=True)
-
-# get the chunk sizes and their mean variances
-chunk_sizes = list(set(df_variances['length_of_chunk']))
-for i,c in enumerate(chunk_sizes):
-    chunk_sizes[i] = int(c)
-mean_chunk_variances = []
-# calculate the mean variance for each chunk size
-for size in chunk_sizes:
-    mean_chunk_variances.append(np.mean(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
-
-# scatter them nicely
-plt.scatter(chunk_sizes, mean_chunk_variances)
-plt.gcf().set_size_inches(14, 7)
-plt.ylabel('mean variance of the GFI over all documents')
-plt.xlabel('length of chunk in sentences')
-plt.savefig('Plots/homework/variances.svg')
--- plottingTheses.py
+++ plottingTheses.py
@@ -4,79 +4,126 @@ import matplotlib.pyplot as plt
 
 
 
-df = pd.read_csv("resultsTheses.csv", header=0, index_col=0)
-all_theses = ['en114417450.txt', 'en119716549.txt', 'en119767323.txt', 'en122940506.txt', 'en116249615.txt', 'en115002482.txt', 'en117652377.txt', 'en117661421.txt']
-
 # BOXPLOT
 # show all the boxplots of all documents' GFIs for chunk size m next to each other
-for m in [1000, 750, 500, 450, 400, 350, 300, 250, 200, 150, 100, 75, 50, 40, 30, 20, 10]:
-    my_doc_chunk_m = df.loc[df['length_of_chunk'] == m]
-    my_doc_chunk_m.boxplot(column='GFI', by='document', figsize=(14,7))
-    plt.ylabel('GFI')
-    plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
-    plt.savefig('Plots/thesis/boxplot/' + str(m) + 'longChunksBoxplottedAllTheses.svg')
+def plot_boxplots():
+    for m in [1000, 750, 500, 450, 400, 350, 300, 250, 200, 150, 100, 75, 50, 40, 30, 20, 10]:
+        my_doc_chunk_m = df.loc[df['length_of_chunk'] == m]
+        my_doc_chunk_m.boxplot(column='GFI', by='document', figsize=(14,7))
+        plt.ylabel('GFI')
+        plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
+        plt.savefig('Plots/thesis/boxplot/' + str(m) + 'longChunksBoxplottedAllTheses.svg')
 
 
 # BAR CHART
-for m in [100, 75, 50, 40, 30, 20, 10]:
-    for thesis in all_theses:
-        plt.close('all')
-        this_doc_len_m = df.loc[df['document']==thesis].loc[df['length_of_chunk']==m]
-        if this_doc_len_m.empty == True:
-            continue
-        ints = []
-        for i in this_doc_len_m['number_of_chunk']:
-            ints.append(int(i))
-        plt.bar(ints,this_doc_len_m['GFI'])
-        plt.gcf().set_size_inches(7, 3.5)
-        plt.ylabel('GFI')
-        plt.xlabel('number of chunk (from beginning to end)')
-        plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
-        plt.savefig('Plots/thesis/bar/' + str(m) + "_long_" + thesis + 'indicesOverNumberOfChunk.svg')
+def plot_barcharts():
+    for m in [100, 75, 50, 40, 30, 20, 10]:
+        for thesis in all_theses:
+            plt.close('all')
+            this_doc_len_m = df.loc[df['document']==thesis].loc[df['length_of_chunk']==m]
+            if this_doc_len_m.empty == True:
+                continue
+            ints = []
+            for i in this_doc_len_m['number_of_chunk']:
+                ints.append(int(i))
+            plt.bar(ints,this_doc_len_m['GFI'])
+            plt.gcf().set_size_inches(7, 3.5)
+            plt.ylabel('GFI')
+            plt.xlabel('number of chunk (from beginning to end)')
+            plt.figtext(0.995, 0.01, 'the chunk size is ' + str(m), ha='right', va='bottom')
+            plt.savefig('Plots/thesis/bar/' + str(m) + "_long_" + thesis + 'indicesOverNumberOfChunk.svg')
 
 
 # SCATTER PLOT
-for thesis in all_theses:
-    plt.close('all')
-    this_doc = df.loc[df['document']==thesis]
+def plot_scatterplot():
+    for thesis in all_theses:
+        plt.close('all')
+        this_doc = df.loc[df['document']==thesis]
 
-    # scatter the GFIs depending on the chunk size
-    plt.scatter(this_doc['length_of_chunk'], this_doc['GFI'])
-    plt.gcf().set_size_inches(14, 7)
+        # scatter the GFIs depending on the chunk size
+        plt.scatter(this_doc['length_of_chunk'], this_doc['GFI'])
+        plt.gcf().set_size_inches(14, 7)
 
-    # calculate the mean GFIs for each chunk size
-    mean_GFIs = { 'len' : [],
-                  'mean' : []}
-    for l in sorted(set(this_doc['length_of_chunk'])):
-        values = this_doc.loc[this_doc['length_of_chunk'] == l]['GFI']
-        mean_GFIs['len'].append(l)
-        mean_GFIs['mean'].append(np.mean(values))
+        # calculate the mean GFIs for each chunk size
+        mean_GFIs = { 'len' : [],
+                      'mean' : []}
+        for l in sorted(set(this_doc['length_of_chunk'])):
+            values = this_doc.loc[this_doc['length_of_chunk'] == l]['GFI']
+            mean_GFIs['len'].append(l)
+            mean_GFIs['mean'].append(np.mean(values))
 
-    # insert the means into the plot
-    plt.plot(mean_GFIs['len'], mean_GFIs['mean'], 'k-', color='r')
-    plt.ylabel('GFI')
-    plt.xlabel('length of chunk in sentences')
-    plt.savefig('Plots/thesis/scatter/' + thesis + 'indicesOverChunkSize.svg')
+        # insert the means into the plot
+        plt.plot(mean_GFIs['len'], mean_GFIs['mean'], 'k-', color='r')
+        plt.ylabel('GFI')
+        plt.xlabel('length of chunk in sentences')
+        plt.savefig('Plots/thesis/scatter/' + thesis + 'indicesOverChunkSize.svg')
 
-
-plt.close('all')
 # VARIANCES
 # display variances of chunks
 # read in the variances from the file
-df_variances = pd.read_csv("variancesTheses.csv", header=0, index_col=0)
+def plot_variances():
+    plt.close('all')
+    df_variances = pd.read_csv("variancesTheses.csv", header=0, index_col=0)
 
-# get the chunk sizes and their mean variances
-chunk_sizes = list(set(df_variances['length_of_chunk']))
-for i,c in enumerate(chunk_sizes):
-    chunk_sizes[i] = int(c)
-mean_chunk_variances = []
-# calculate the mean variance for each chunk size
-for size in chunk_sizes:
-    mean_chunk_variances.append(np.mean(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
+    # get the chunk sizes and their mean variances
+    chunk_sizes = list(sorted(set(df_variances['length_of_chunk'])))
+    for i,c in enumerate(chunk_sizes):
+        chunk_sizes[i] = int(c)
+    mean_chunk_variances = []
+    median_chunk_variances = []
+    # calculate the mean variance for each chunk size
+    for size in chunk_sizes:
+        mean_chunk_variances.append(np.mean(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
+        median_chunk_variances.append(np.median(df_variances.loc[df_variances['length_of_chunk'] == size]['variance']))
 
-# scatter them nicely
-plt.scatter(chunk_sizes, mean_chunk_variances)
-plt.gcf().set_size_inches(14, 7)
-plt.ylabel('mean variance of the GFI over all documents')
-plt.xlabel('length of chunk in sentences')
-plt.savefig('Plots/thesis/variancesOnChunkSize.svg')
+
+    # scatter them nicely
+    plt.scatter(chunk_sizes, mean_chunk_variances, label = 'mean values')
+    plt.scatter(chunk_sizes, median_chunk_variances, label = 'median values')
+    plt.legend()
+    plt.gcf().set_size_inches(14, 7)
+    plt.ylabel('mean variance of the GFI over all documents')
+    plt.xlabel('length of chunk in sentences')
+    plt.savefig('Plots/thesis/variancesOnChunkSize.svg')
+
+# DEVIATIONS
+# display standard deviations of chunks
+# read in the deviations from the file
+def plot_deviations():
+    plt.close('all')
+    df_deviations = pd.read_csv("variancesTheses.csv", header=0, index_col=0)
+
+    # get the chunk sizes and their mean variances
+    chunk_sizes = list(sorted(set(df_deviations['length_of_chunk'])))
+    for i,c in enumerate(chunk_sizes):
+        chunk_sizes[i] = int(c)
+    mean_chunk_deviations = []
+    median_chunk_deviations = []
+    # calculate the mean variance for each chunk size
+    for size in chunk_sizes:
+        mean_chunk_deviations.append(np.mean(df_deviations.loc[df_deviations['length_of_chunk'] == size]['std']))
+        median_chunk_deviations.append(np.median(df_deviations.loc[df_deviations['length_of_chunk'] == size]['std']))
+
+
+    # scatter them nicely
+    plt.scatter(chunk_sizes, mean_chunk_deviations, label = 'mean values')
+    plt.scatter(chunk_sizes, median_chunk_deviations, label = 'median values')
+    plt.legend()
+    plt.gcf().set_size_inches(14, 7)
+    plt.ylabel('standard deviation of the GFI over all documents')
+    plt.xlabel('length of chunk in sentences')
+    plt.savefig('Plots/thesis/deviationsOnChunkSize.svg')
+
+
+
+df = pd.read_csv("resultsTheses.csv", header=0, index_col=0)
+all_theses = ['en114417450.txt', 'en119716549.txt', 'en119767323.txt', 'en122940506.txt', 'en116249615.txt', 'en115002482.txt', 'en117652377.txt', 'en117661421.txt']
+
+plot_boxplots()
+plot_barcharts()
+plot_scatterplot()
+plot_variances()
+plot_deviations()
+
+plt.close('all')
+
